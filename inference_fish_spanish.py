6ce2725e61fb18773278290a5e3fc783cfc0eba6
Manuel Sandoval <msandovalgm@live.com.mx>
2026-01-23 00:15:14 -0600

    Voice adjustments

--- inference_fish_spanish.py
+++ inference_fish_spanish.py
@@ -89,7 +89,7 @@ VOICE_PRESETS = {
         universo, como le quieras llamar. Todos somos parte de eso. Nacemos y nos convertimos en esto por un ratito 
         muy chiquito, muy chiquitito, que creemos que es muy largo y se nos olvida que vamos a regresar a ese lugar 
         de donde venimos, que es lo que tú creas, adonde tú creas, pero inevitablemente vas a regresar.""",
-        "style_tags": "(calm) (narrator)"
+        "style_tags": "(calm)"
     }
     # "CRISTINA": {
     #     "temp": 0.75,
